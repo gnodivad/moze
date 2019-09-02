20afda72c7ef5effa616f13bea833228042c1ea1
David <dodwei.dev@gmail.com>
2019-09-02 16:32:12 +0800

    Convert display all accounts to endpoint

--- app.js
+++ app.js
@@ -1,18 +1,28 @@
 const realm = require('realm');
 const model = require('./model.js');
+const express = require('express')
+const app = express()
+const port = 3000
 
 const currentVersion = Realm.schemaVersion('moze.realm');
 
-realm.open({
-    path: 'moze.realm',
-    schema: [model.AHAccount, model.AHAccountGroup, model.AHCurrency],
-    schemaVersion: currentVersion
-}).then(realm => {
-    let accounts = realm.objects('AHAccount');
+app.get('/accounts', (req, res) => {
+    realm.open({
+        path: 'moze.realm',
+        schema: [model.AHAccount, model.AHAccountGroup, model.AHCurrency],
+        schemaVersion: currentVersion
+    }).then(realm => {
+        let accounts = realm.objects('AHAccount');
 
-    for (let p of accounts) {
-        console.log(p.name);
-    }
+        res.send({ data: realmToJson(accounts) });
 
-    realm.close();
+        realm.close();
+    })
 })
+
+function realmToJson(realmObject) {
+    return Array.from(realmObject);
+}
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
